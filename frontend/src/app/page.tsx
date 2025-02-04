d53f813cf1e6710bb6f72abf3d1caf28fc5bf494
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2025-02-04 23:24:06 +0100

    added: france map

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,16 @@
+import CarteFrance from "@/components/CarteFrance";
+
 export default function Home() {
   return (
-    <div className="bg-white w-full rounded-2xl border border-gray-300 shadow-md p-4 mb-7">
+    <div className="bg-white w-full rounded-2xl border border-gray-300 shadow-md p-4 mb-7 flex">
       <div className="w-1/2 h-full flex justify-center items-center p-10">
         <p className="text-5xl">
           Trouvez facilement les aires de covoiturage près de chez-vous
         </p>
       </div>
-      <div className="w-1/2"></div>
+      <div className="w-1/2 h-full flex justify-center items-center p-10">
+        <CarteFrance />
+      </div>
     </div>
   );
 }
